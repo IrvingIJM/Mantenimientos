49cddae0d4dafad17673ac95ac514d2e01beec46
irvingj0609@gmail.com <irvingj0609@gmail.com>
2026-05-22 17:55:26 -0600

    services

---
+++ Mantenimientos/Services/EmpDataService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Mantenimientos.Services
+{
+    public class EmpDataService
+    {
+        private readonly string? _connectionString;
+        private readonly ILogger<EmpDataService> _logger;
+
+        private static readonly DateTime FechaDefault = new(1900, 1, 1);
+        public EmpDataService(
+            IConfiguration configuracion,
+            ILogger<EmpDataService> logger)
+        {
+            _connectionString = configuracion.GetConnectionString("DatabaseConnection") ?? throw new InvalidOperationException(
+                "No se encontro 'DatabaseConnection' en appsettings.json");
+            _logger = logger;
+        }
+
+    }
+}
