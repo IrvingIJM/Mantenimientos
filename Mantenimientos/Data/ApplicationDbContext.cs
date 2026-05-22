f21dd8545ba9409e638970a229cda5978a8a5b27
irvingj0609@gmail.com <irvingj0609@gmail.com>
2026-05-22 16:49:14 -0600

    Archivo ApplicationDbContext modificado

--- Mantenimientos/Data/ApplicationDbContext.cs
+++ Mantenimientos/Data/ApplicationDbContext.cs
@@ -1,6 +1,33 @@
-﻿namespace Mantenimientos.Data
+﻿using DocumentFormat.OpenXml.Vml.Office;
+using Mantenimientos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mantenimientos.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        //Tablas
+        public DbSet<Seguimiento> Seguimientos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Seguimiento>(entity =>
+            entity.ToTable("Seguimientos"),
+            entity.Property(e => e.RUTA).IsRequired().HasMaxLenght(100),
+            entity.Property(e => e.SUCURSAL).IsRequired().HasMaxLenght(200),
+            entity.Property(e => e.FECHA_INI_ES).HasColumnType("date").HasDefaultValue(new DateTime(1900, 1, 1)),
+            entity.Property(e => e.FECHA_FIN_ES).HasColumnType("date").HasDefaultValue(new DateTime(1900, 1, 1)),
+            entity.Property(e => e.FECHA_INI_RE).HasColumnType("date").HasDefaultValue(new DateTime(1900, 1, 1)),
+            entity.Property(e => e.FECHA_FIN_RE).HasColumnType("date").HasDefaultValue(new DateTime(1900, 1, 1)),
+            entity.Property(e => e.DIAS_ATRASO).HasDefaultValue(0),
+            entity.Property(e => e.OBSERVACIONES).HasColumnType("nvarchar(max)"),
+
+            //Indices para busquedas frecuentes
+            entity.HasIndex(e => e.RUTA).HasDatabaseName("IX_Seguimiento_Ruta"),
+            entity.HasIndex(e => e.SUCURSAL).HasDatabaseName("IX_Seguimiento_Sucursal"),
+            entity.HasIndex(e => e.FECHA_INI_ES).HasDatabaseName("IX_Seguimiento_FechaIniEst"));
+        }
     }
 }
